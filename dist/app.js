dcaaf5b0fac8a901d7ecfff76c2ad43a63443520
user <user@ken.local>
2022-10-26 03:05:52 +0900

    add interface in Class

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
+class Person {
+    constructor(n, age) {
+        this.age = age;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
     }
-};
+}
+let user1;
+user1 = new Person('Max', 30);
+console.log(user1);
 user1.greet('hello I am');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
-interface Person {
+interface Greetable {
   // どのような構造なのかという形のみを書く
   name: string;
-  age: number;
 
   // 引数、返り値を書く
   greet(phrase: string): void;
 }
 
-// user1にPerson型と同じ構造を割り当てたい
-let user1: Person;
+// personクラスはGreetableというインターフェースに従って実装するよ
+class Person implements Greetable {
+    name: string;
+    constructor(n: string, public age: number) {
+        this.name = n;
+    }
 
-// user1をPersonに従って定義
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name)
+    greet(phrase: string ) {
+        console.log(phrase + ' ' + this.name);
     }
 }
 
+// user1にPerson型と同じ構造を割り当てたい
+let user1: Greetable;
+
+// user1をPersonに従って定義
+user1 = new Person('Max', 30)
+console.log(user1)
 user1.greet('hello I am')
