45dd1837341a4e375969ac13e6d26070781cc489
user <user@ken.local>
2022-10-26 02:51:09 +0900

    add interface

--- dist/app.js
+++ dist/app.js
@@ -1,81 +1,11 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
+let user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-Department.fiscalYear = 2020;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-    describe() {
-        console.log('IT部門 - ID: ' + this.id);
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('レポートが見つかりません');
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error('正しい値を設定してください');
-        }
-        this.addReport(value);
-    }
-    static getInstance() {
-        if (AccountingDepartment.instance) {
-            return AccountingDepartment.instance;
-        }
-        this.instance = new AccountingDepartment('d2', []);
-        return this.instance;
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-    addEmployee(name) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name);
-    }
-    describe() {
-        console.log('会計部門 - ID: ' + this.id);
-    }
-}
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-const employee1 = Department.createEmployee('Max');
-console.log(employee1, Department.fiscalYear);
-accounting.mostRecentReport = '通期会計レポート';
-accounting.addReport('something');
-console.log(accounting.mostRecentReport);
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-accounting.describe();
+};
+user1.greet('hello I am');
 //# sourceMappingURL=app.js.map
