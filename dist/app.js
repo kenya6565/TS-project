490531e5bdc829db9776231aa2f898f6cfe92127
user <user@ken.local>
2022-10-27 23:38:14 +0900

    add type guard

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,45 @@ const e1 = {
     privileges: ['create-server'],
     startDate: new Date
 };
+function add2(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+const a = add2(2, 3);
+console.log(a);
+function printEmployeeInformation(emp) {
+    if ('privileges' in emp) {
+        console.log('Privileges ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('start Date: ' + emp.startDate);
+    }
+    console.log(emp.name);
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('運転中...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('トラックを運転中...');
+    }
+    loadCargo(amount) {
+        console.log('荷物を乗せています...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,68 @@ type Numeric = number | boolean;
 
 // CombinableとNumericで共通しているのがnumberなのでnumber型になる
 type Universal = Combinable & Numeric
+
+
+function add2(a: Combinable, b: Combinable) {
+    // 型によって処理を切り替える→型がーど
+    // aかbどちらかがstringであれば文字列化する
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b;
+}
+
+const a = add2(2, 3)
+console.log(a)
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    if ('privileges' in emp) {
+        console.log('Privileges ' + emp.privileges)
+    }
+
+    if('startDate' in emp) {
+        console.log('start Date: ' + emp.startDate)
+    }
+    console.log(emp.name);
+    
+}
+
+
+printEmployeeInformation(e1)
+
+class Car {
+    drive() {
+        console.log('運転中...')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('トラックを運転中...')
+    }
+
+    loadCargo(amount: number) {
+        console.log('荷物を乗せています...' + amount)
+    }
+}
+
+
+type Vehicle = Car | Truck
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    // Truckのインスタンスから作成されているものか判断する
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
+
