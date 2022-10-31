6526d57a0a2eced98fa1646efa1b24742ae92739
user <user@ken.local>
2022-11-01 03:09:26 +0900

    add another generics

--- dist/app.js
+++ dist/app.js
@@ -3,4 +3,12 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Max', hobbies: ['sports'] }, { age: 30 });
+function countAndDescribe(element) {
+    let descriptionText = '値がありません';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(['hoge', 'fuga']));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,17 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 //型推論でobject型ということを明確に伝えなくてもよい
 const mergedObj = merge({ name: 'Max', hobbies: ['sports'] }, { age: 30 });
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = '値がありません'
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です'
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe(['hoge', 'fuga']))
